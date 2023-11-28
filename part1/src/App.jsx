d4b62cb4ab11dbc2a4aa21ef2564aa83ec288f61
haster-tabloids <sudeshmate35@gmail.com>
2023-11-28 22:00:40 +0545

    feat:using useState hook in react

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,34 +1,15 @@
-// const Hello = ({ name, age }) => {
-//   const bornYear = () => new Date().getFullYear() - age
+import { useState } from 'react'
 
-//   return (
-//     <div>
-//       <p>
-//         Hello {name}, you are {age} years old
-//       </p>
-//       <p>So you were probably born in {bornYear()}</p>
-//     </div>
-//   )
-// }
+const App = () => {
 
-// const App = () => {
-//   const name = 'Peter'
-//   const age = 10
-
-//   return (
-//     <div>
-//       <h1>Greetings</h1>
-//       <Hello name="Maya" age={26 + 10} />
-//       <Hello name={name} age={age} />
-//     </div>
-//   )
-// }
-
-// export default App;
+  const [ counter, setCounter ] = useState(0)
 
 
-const App = (props) => {
-  const {counter} = props
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
+
   return (
     <div>{counter}</div>
   )
