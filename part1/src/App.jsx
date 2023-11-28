728ccfa05f3750b741ccdf45ef70e8f360053e83
haster-tabloids <sudeshmate35@gmail.com>
2023-11-28 22:53:01 +0545

    feat:Passing state - to child components

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,23 +1,30 @@
+import Display from './Components/Display'
+import Button from './Components/Button'
 import { useState } from 'react'
 
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
   const increaseByOne = () => setCounter(counter + 1)
-  
+  const decreaseByOne = () => setCounter(counter - 1)
   const setToZero = () => setCounter(0)
   
 
   return (
     <div>
-      <div>{counter}</div>
-
-      <button onClick={increaseByOne}>
-        plus
-      </button>
-      <button onClick={setToZero}> 
-        zero
-      </button>
+      <Display counter={counter}/>
+      <Button
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        onClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />    
     </div>
   )
 }
