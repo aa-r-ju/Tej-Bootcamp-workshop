fa3b9a44f3dd8b6a83a4568af82db36128800cac
haster-tabloids <sudeshmate35@gmail.com>
2023-11-28 21:23:36 +0545

    feat:try page rerendering method

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,28 +1,37 @@
-const Hello = (props) => {
-  const { name, age } = props
-  const bornYear = () => new Date().getFullYear() - age
+// const Hello = ({ name, age }) => {
+//   const bornYear = () => new Date().getFullYear() - age
 
+//   return (
+//     <div>
+//       <p>
+//         Hello {name}, you are {age} years old
+//       </p>
+//       <p>So you were probably born in {bornYear()}</p>
+//     </div>
+//   )
+// }
+
+// const App = () => {
+//   const name = 'Peter'
+//   const age = 10
+
+//   return (
+//     <div>
+//       <h1>Greetings</h1>
+//       <Hello name="Maya" age={26 + 10} />
+//       <Hello name={name} age={age} />
+//     </div>
+//   )
+// }
+
+// export default App;
+
+
+const App = (props) => {
+  const {counter} = props
   return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
+    <div>{counter}</div>
   )
 }
 
-const App = () => {
-  const name = 'Peter'
-  const age = 10
-
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-    </div>
-  )
-}
-
-export default App;
+export default App
