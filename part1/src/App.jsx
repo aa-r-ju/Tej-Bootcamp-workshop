1cbba6d36cd6c09b45a094bb2efb90b120dc6d66
haster-tabloids <sudeshmate35@gmail.com>
2023-11-30 18:32:24 +0545

    feat:using two useState

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,30 +1,19 @@
-import Display from './Components/Display'
-import Button from './Components/Button'
 import { useState } from 'react'
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
-
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
-  
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
 
   return (
     <div>
-      <Display counter={counter}/>
-      <Button
-        onClick={increaseByOne}
-        text='plus'
-      />
-      <Button
-        onClick={setToZero}
-        text='zero'
-      />     
-      <Button
-        onClick={decreaseByOne}
-        text='minus'
-      />    
+      {left}
+      <button onClick={() => setLeft(left + 1)}>
+        left
+      </button>
+      <button onClick={() => setRight(right + 1)}>
+        right
+      </button>
+      {right}
     </div>
   )
 }
