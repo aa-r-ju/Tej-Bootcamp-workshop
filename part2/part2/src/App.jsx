b6ca7e0870c5d261d284208226b679e863e40e42
aa-r-ju <aarju3447@gmail.com>
2023-12-04 10:31:20 +0545

    feat:using useState in part2-b workshop

--- part2/part2/src/App.jsx
+++ part2/part2/src/App.jsx
@@ -1,10 +1,14 @@
-const App = ({ notes }) => {
-
+import { useState } from 'react'
+import Note from "./Components/Note"
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note => <li key={note.id}>{note.content}</li>)}
+      {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
       </ul>
     </div>
   )
