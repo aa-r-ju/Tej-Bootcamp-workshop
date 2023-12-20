9caf09e5be10a8b04cbe50aac70fb917511ef0e9
aa-r-ju <aarju3447@gmail.com>
2023-12-20 11:37:22 +0545

    feat:doing part3-b workshop

--- part3/note-server/index.js
+++ part3/note-server/index.js
@@ -37,10 +37,6 @@ let notes = [
 
   app.use(requestLogger)
 
-
-  // app.get('/', (request, response) => {
-  //   response.send('<h1>Hello World!</h1>')
-  // })
   
   app.get('/api/notes', (request, response) => {
     response.json(notes)
@@ -107,6 +103,6 @@ let notes = [
 
 
 
-const PORT = 3001
+const PORT = process.env.PORT ? process.env.PORT : 3001;
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
